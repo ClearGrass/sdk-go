cc89e1ff1e1a6f3f824d4a33fdadf5e9c162c728
fizzse <song.song@qingping.co>
2024-05-14 10:21:37 +0800

    example

--- encoding/tlv_test.go
+++ encoding/tlv_test.go
@@ -1,1 +1,24 @@
 package encoding
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"testing"
+)
+
+func TestTlvEncode_ReadingOffset(t *testing.T) {
+	msg := NewMessagePod(true)
+
+	msg.CmdType = 50
+	msg.ReadingOffsetSetting = &ReadingOffsetSetting{
+		Temperature: &ReadingOffset{OffsetValue: 2},
+	}
+
+	ret, err := TlvEncode(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(hex.EncodeToString(ret))
+}
