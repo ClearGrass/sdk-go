498db5792d8d16331bc30206cae3b7dc12eb1732
fizzse <song.song@qingping.co>
2024-08-14 10:57:23 +0800

    example

--- encoding/lora_test.go
+++ encoding/lora_test.go
@@ -1,7 +1,6 @@
 package encoding
 
 import (
-	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
@@ -10,13 +9,14 @@ import (
 
 func TestDecodeLoraRobbData(t *testing.T) {
 	hexDataList := []string{
-		"01412401662758412FF2F5000003F6000D000E005C000C00000146FF000C3ABDCD00312E302E36EA05",
-		"01411B0066276E0403842FF2F5000003F6000D000E005C000C00000146FF9076",
+		//"01412401662758412FF2F5000003F6000D000E005C000C00000146FF000C3ABDCD00312E302E36EA05",
+		//"01411B0066276E0403842FF2F5000003F6000D000E005C000C00000146FF9076",
+		"01411300664312bc003c31f1fb075e6232517507ae628ca7",
 	}
 
-	base64DataList := []string{
-		"AUEkAWYq9Tgu8isAAAILAAYABgBsACYAAAG8/WA0MZYWADEuMS4weNA=",
-	}
+	//base64DataList := []string{
+	//	"AUEkAWYq9Tgu8isAAAILAAYABgBsACYAAAG8/WA0MZYWADEuMS4weNA=",
+	//}
 
 	for _, src := range hexDataList {
 
@@ -28,12 +28,12 @@ func TestDecodeLoraRobbData(t *testing.T) {
 		fmt.Println(string(c))
 	}
 
-	for _, src := range base64DataList {
-		bs, _ := base64.StdEncoding.DecodeString(src)
-		fmt.Println(hex.EncodeToString(bs))
-
-		out, _ := DecodeLoraRobbData(bs)
-		c, _ := json.Marshal(out)
-		fmt.Println(string(c))
-	}
+	//for _, src := range base64DataList {
+	//	bs, _ := base64.StdEncoding.DecodeString(src)
+	//	fmt.Println(hex.EncodeToString(bs))
+	//
+	//	out, _ := DecodeLoraRobbData(bs)
+	//	c, _ := json.Marshal(out)
+	//	fmt.Println(string(c))
+	//}
 }
--- example/encoding/tlv_decode.go
+++ example/encoding/tlv_decode.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 
 	"github.com/ClearGrass/sdk-go/encoding"
 )
 
-func main() {
+func exampleDecodeData() {
 	encodeData := "Q0c0QgA4AgA8ABEFADIuMC4zIgQAMDAwMCwBAAE0BQAxLjMuNzUFADIuMC4zHQEAARQTAMXA82VJ4i8EHAAAADICCQEAzwCWCw=="
 
 	dataBytes, err := base64.StdEncoding.DecodeString(encodeData)
@@ -19,4 +20,25 @@ func main() {
 	data, err := encoding.TlvDecode(dataBytes)
 	c, _ := json.Marshal(data)
 	fmt.Println(string(c))
+}
+
+func exampleSetIntervalCmd() {
+	msg := encoding.NewMessagePod(true)
+
+	msg.CmdType = 0x32
+	msg.IntervalSetting = &encoding.IntervalSetting{CollectInterval: 60, ReportInterval: 60}
+	msg.SetEndFlag(1)
+
+	ret, err := encoding.TlvEncode(msg)
+	if err != nil {
+
+	}
+
+	fmt.Println(hex.EncodeToString(ret))
+	fmt.Println(base64.StdEncoding.EncodeToString(ret))
+}
+
+func main() {
+	//exampleDecodeData()
+	exampleSetIntervalCmd()
 }
